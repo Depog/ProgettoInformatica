4a58ae4045cf3e2d9d7b305f8241c3c330116af2
Fede1019 <60525792+Fede1019@users.noreply.github.com>
2020-04-15 23:00:35 +0200

    Login

---
+++ esPHP/InnovativeBuzzi/login/connessione.php
@@ -0,0 +1,17 @@
+<?php
+
+	$servername = "localhost";
+	$usernameDb = "root";
+	$passwordDb = "";
+	$dbNome="innovativeBuzzi";   //nome db
+	// Create connection
+	$conn = new mysqli($servername, $usernameDb, $passwordDb,$dbNome);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connessione avvenuta con successo";
+
+
+?>
