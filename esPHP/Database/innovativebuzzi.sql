06a354dce0f4910810d6846cbfd948452714b21e
Fede1019 <60525792+Fede1019@users.noreply.github.com>
2020-04-18 14:52:41 +0200

    database

---
+++ esPHP/Database/innovativebuzzi.sql
@@ -0,0 +1,289 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 18, 2020 alle 14:51
+-- Versione del server: 10.4.11-MariaDB
+-- Versione PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `innovativebuzzi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `acquisto`
+--
+
+CREATE TABLE `acquisto` (
+  `idAcquisto` int(11) NOT NULL,
+  `codiceFiscale` char(16) DEFAULT NULL,
+  `dataAcquisto` date DEFAULT NULL,
+  `orarioAcquisto` time DEFAULT NULL,
+  `Quantità` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `file`
+--
+
+CREATE TABLE `file` (
+  `idFile` int(11) NOT NULL,
+  `nomeFile` varchar(128) DEFAULT NULL,
+  `dimensione` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `formato`
+--
+
+CREATE TABLE `formato` (
+  `Tipo` varchar(16) NOT NULL,
+  `costoStampa` decimal(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `include`
+--
+
+CREATE TABLE `include` (
+  `idAcquisto` int(11) NOT NULL,
+  `idProdotto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `persona`
+--
+
+CREATE TABLE `persona` (
+  `codiceFiscale` char(16) NOT NULL,
+  `nome` varchar(32) DEFAULT NULL,
+  `cognome` varchar(32) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `username` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `tipo` varchar(64) DEFAULT NULL,
+  `dataNascita` date DEFAULT NULL,
+  `civico` int(11) DEFAULT NULL,
+  `cap` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `persona`
+--
+
+INSERT INTO `persona` (`codiceFiscale`, `nome`, `cognome`, `password`, `username`, `email`, `tipo`, `dataNascita`, `civico`, `cap`) VALUES
+('', NULL, NULL, '9d04b6572e137eb28b2c444c1c7d3faf', 'Fede', NULL, 'Professore\r\n', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prenotazione`
+--
+
+CREATE TABLE `prenotazione` (
+  `idPrenotazione` int(11) NOT NULL,
+  `dataPrenotazione` date DEFAULT NULL,
+  `oraPrenotazione` time DEFAULT NULL,
+  `quantità` int(11) DEFAULT NULL,
+  `stampata` enum('si','no') DEFAULT 'no',
+  `codiceFiscaleCliente` char(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prodotto`
+--
+
+CREATE TABLE `prodotto` (
+  `idProdotto` int(11) NOT NULL,
+  `nomeProdotto` varchar(64) DEFAULT NULL,
+  `costoProdotto` decimal(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stampa`
+--
+
+CREATE TABLE `stampa` (
+  `idStampa` int(11) NOT NULL,
+  `dataStampa` date DEFAULT NULL,
+  `oraStampa` time DEFAULT NULL,
+  `codiceFiscalePersona` char(16) DEFAULT NULL,
+  `dataRitiro` date DEFAULT NULL,
+  `oraRitiro` time DEFAULT NULL,
+  `tipoFormato` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `studente`
+--
+
+CREATE TABLE `studente` (
+  `codiceFiscale` char(16) NOT NULL,
+  `classe` char(3) DEFAULT NULL,
+  `sezione` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `acquisto`
+--
+ALTER TABLE `acquisto`
+  ADD PRIMARY KEY (`idAcquisto`),
+  ADD KEY `codiceFiscale` (`codiceFiscale`);
+
+--
+-- Indici per le tabelle `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`idFile`);
+
+--
+-- Indici per le tabelle `formato`
+--
+ALTER TABLE `formato`
+  ADD PRIMARY KEY (`Tipo`);
+
+--
+-- Indici per le tabelle `include`
+--
+ALTER TABLE `include`
+  ADD PRIMARY KEY (`idAcquisto`,`idProdotto`),
+  ADD KEY `idProdotto` (`idProdotto`);
+
+--
+-- Indici per le tabelle `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`codiceFiscale`);
+
+--
+-- Indici per le tabelle `prenotazione`
+--
+ALTER TABLE `prenotazione`
+  ADD PRIMARY KEY (`idPrenotazione`),
+  ADD KEY `codiceFiscaleCliente` (`codiceFiscaleCliente`);
+
+--
+-- Indici per le tabelle `prodotto`
+--
+ALTER TABLE `prodotto`
+  ADD PRIMARY KEY (`idProdotto`);
+
+--
+-- Indici per le tabelle `stampa`
+--
+ALTER TABLE `stampa`
+  ADD PRIMARY KEY (`idStampa`),
+  ADD KEY `codiceFiscalePersona` (`codiceFiscalePersona`),
+  ADD KEY `tipoFormato` (`tipoFormato`);
+
+--
+-- Indici per le tabelle `studente`
+--
+ALTER TABLE `studente`
+  ADD PRIMARY KEY (`codiceFiscale`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `acquisto`
+--
+ALTER TABLE `acquisto`
+  MODIFY `idAcquisto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `file`
+--
+ALTER TABLE `file`
+  MODIFY `idFile` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `prenotazione`
+--
+ALTER TABLE `prenotazione`
+  MODIFY `idPrenotazione` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `prodotto`
+--
+ALTER TABLE `prodotto`
+  MODIFY `idProdotto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `stampa`
+--
+ALTER TABLE `stampa`
+  MODIFY `idStampa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `acquisto`
+--
+ALTER TABLE `acquisto`
+  ADD CONSTRAINT `acquisto_ibfk_1` FOREIGN KEY (`codiceFiscale`) REFERENCES `persona` (`codiceFiscale`) ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `include`
+--
+ALTER TABLE `include`
+  ADD CONSTRAINT `include_ibfk_1` FOREIGN KEY (`idAcquisto`) REFERENCES `acquisto` (`idAcquisto`),
+  ADD CONSTRAINT `include_ibfk_2` FOREIGN KEY (`idProdotto`) REFERENCES `prodotto` (`idProdotto`) ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `prenotazione`
+--
+ALTER TABLE `prenotazione`
+  ADD CONSTRAINT `prenotazione_ibfk_1` FOREIGN KEY (`codiceFiscaleCliente`) REFERENCES `persona` (`codiceFiscale`) ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `stampa`
+--
+ALTER TABLE `stampa`
+  ADD CONSTRAINT `stampa_ibfk_1` FOREIGN KEY (`codiceFiscalePersona`) REFERENCES `persona` (`codiceFiscale`),
+  ADD CONSTRAINT `stampa_ibfk_2` FOREIGN KEY (`tipoFormato`) REFERENCES `formato` (`Tipo`) ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `studente`
+--
+ALTER TABLE `studente`
+  ADD CONSTRAINT `studente_ibfk_1` FOREIGN KEY (`codiceFiscale`) REFERENCES `persona` (`codiceFiscale`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
